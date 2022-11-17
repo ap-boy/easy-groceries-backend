710c5a96229c166d83128556075a4a86212e560b
Andrew Boy <andrew.boy@entelect.co.uk>
2022-11-17 22:15:53 +0000

    add db context

---
+++ Data/EasyGroceryContext.cs
@@ -0,0 +1,13 @@
+﻿using easy_groceries_backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace easy_groceries_backend.Data
+{
+    public class EasyGroceryContext : DbContext
+    {
+        public EasyGroceryContext(DbContextOptions<EasyGroceryContext> context) : base(context)
+        {
+        }
+        public DbSet<StoreItem> StoreItem { get; set; }
+    }
+}
