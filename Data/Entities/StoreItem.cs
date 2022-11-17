8060f997a7a453bf0d131f2021511f58da7b1146
Andrew Boy <andrew.boy@entelect.co.uk>
2022-11-17 22:16:06 +0000

    add store item

---
+++ Data/Entities/StoreItem.cs
@@ -0,0 +1,11 @@
+﻿namespace easy_groceries_backend.Data.Entities
+{
+    public class StoreItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
