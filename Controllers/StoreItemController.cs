e6ea76e6ecbea82d88a2c9036fad77bfe5a493f3
Andrew Boy <andrew.boy@entelect.co.uk>
2022-11-17 22:16:23 +0000

    add store item controller

---
+++ Controllers/StoreItemController.cs
@@ -0,0 +1,24 @@
+﻿using easy_groceries_backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace easy_groceries_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreItemController : ControllerBase
+    {
+        private readonly EasyGroceryContext _dbContext;
+        public StoreItemController(EasyGroceryContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var storeItems = await _dbContext.StoreItem.ToListAsync();
+            return Ok(storeItems);
+        }
+    }
+}
